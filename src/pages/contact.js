626472e194a17ee60dd5829e842e2edf5334d60d
andytron <heavyaccents@gmail.com>
2020-11-16 21:07:31 -0500

    Mod: rollback previous Contact page, update redirects list

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import { graphql } from "gatsby"
 import { navigate } from 'gatsby-link'
 
@@ -13,18 +13,24 @@ function encode(data) {
     .join('&')
 }
 
-const ContactPage = ({ data }) => {
-  const siteTitle = data.site.siteMetadata.title
-  const [state, setState] = useState({})
+class ContactPage extends React.Component {
 
-  const handleInputChange = event => {
-    setState({
-      ...state,
-      [event.target.name]: event.target.value
+  state = {
+    fullName: '',
+    email: '',
+    message: '',
+  }
+
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
     })
   }
 
-  const handleSubmit = event => {
+  handleSubmit = event => {
     event.preventDefault()
     let form = event.target;
 
@@ -33,70 +39,76 @@ const ContactPage = ({ data }) => {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({
         'form-name': form.getAttribute('name'),
-        ...state,
-      }),
+        ...this.state
+      })
     })
       .then(() => navigate(form.getAttribute('action')))
       .catch((error) => alert(error))
   }
 
-  return (
-    <Layout location={{ pathname: '/contact' }} title={siteTitle}>
-      <SEO title="Contact" />
-      <Nav />
-      <div className="post__content">
-        <h2>Contact</h2>
-        <form
-          name="contact"
-          method="POST"
-          action="/thanks/"
-          data-netlify="true"
-          data-netlify-recaptcha="true"
-          netlify-honeypot="bot-field"
-          onSubmit={handleSubmit}
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <input
-            type="hidden"
-            name="bot-field"
-            onChange={handleInputChange}
-          />
-          <label htmlFor="contact-name">Name</label>
-          <input
-            id="contact-name"
-            type="text"
-            name="fullName"
-            placeholder="Name"
-            required
-            onChange={handleInputChange}
-          />
-          <label htmlFor="contact-email">Email</label>
-          <input
-            id="contact-email"
-            type="email"
-            name="email"
-            placeholder="Email"
-            required
-            onChange={handleInputChange}
-          />
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
 
-          <label htmlFor="contact-message">Message</label>
-          <textarea
-            id="contact-message"
-            name="message"
-            placeholder="Message"
-            rows="5"
-            required
-            onChange={handleInputChange}
-          />
-          <div data-netlify-recaptcha="true" />
-          <Button type="submit">
-            Submit
-          </Button>
-        </form>
-      </div>
-    </Layout>
-  )
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="Contact" />
+        <Nav />
+        <div className="post__content">
+          <h2>Contact</h2>
+          <form
+            name="contact-form"
+            method="post"
+            action="/thanks/"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={this.handleSubmit}
+          >
+            <input
+              type="hidden"
+              name="bot-field"
+              onChange={this.handleInputChange}
+            />
+            <input type="hidden" name="form-name" value="contact-form" />
+            <label htmlFor="contact-name">Name</label>
+            <input
+              id="contact-name"
+              type="text"
+              name="fullName"
+              placeholder="Name"
+              required
+              value={this.state.fullName}
+              onChange={this.handleInputChange}
+            />
+            <label htmlFor="contact-email">Email</label>
+            <input
+              id="contact-email"
+              type="email"
+              name="email"
+              placeholder="Email"
+              required
+              value={this.state.email}
+              onChange={this.handleInputChange}
+            />
+
+            <label htmlFor="contact-message">Message</label>
+            <textarea
+              id="contact-message"
+              name="message"
+              value={this.state.message}
+              placeholder="Message"
+              rows="5"
+              required
+              onChange={this.handleInputChange}
+            />
+            <Button type="submit">
+              Submit
+            </Button>
+          </form>
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default ContactPage
